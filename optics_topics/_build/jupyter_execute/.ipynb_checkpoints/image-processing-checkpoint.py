aeed76cc29d47b4a352da418cbcb57a65a648f13
Ben Corner <bcorner@reed.edu>
2022-03-22 06:09:04 -0500

    Add navbar captions; create Image Processing; Optics chapter placeholders

---
+++ optics_topics/_build/jupyter_execute/.ipynb_checkpoints/image-processing-checkpoint.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Image Processing
+# 
+
+# ## Section table of contents
+# ```{tableofcontents}
+# ```
+
+# ## Image Data
+# 
+
+# box of numbers
+# 
+# ![lenna.png](../images/lenna.png)
+# 
+
+# ## Filters/Kernels
+# 
+
+# ## Convolution
+
+# ## Fourier Transform
+
+# In[ ]:
+
+
+
+
---
+++ optics_topics/_build/jupyter_execute/image-processing.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Image Processing
+# 
+
+# ## Section table of contents
+# 
+# ```{tableofcontents}
+# ```
+
+# ## Image Data
+# 
+
+# box of numbers
+# 
+# ![lenna](../images/lenna.png)
+# 
+
+# ## Filters/Kernels
+# 
+
+# ## Convolution
+
+# ## Fourier Transform
+
+# In[ ]:
+
+
+
+
